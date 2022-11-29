2c850e352a554073139967bca37611d01c7883a7
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-11-29 13:15:57 +0100

    Arreglar diálogos de importación y exportación

--- controladores/dialogos/dlgTipoExportacion.py
+++ controladores/dialogos/dlgTipoExportacion.py
@@ -1,13 +1,13 @@
 from PyQt6 import QtWidgets
 
-from ui.dlgTipoImportacion import Ui_dialogoTipoImportacion
+from ui.dlgTipoExportacion import Ui_dialogoTipoExportacion
 
 
 class DialogoTipoExportacion(QtWidgets.QDialog):
 	def __init__(self):
 		super(DialogoTipoExportacion, self).__init__()
-		self.ui = Ui_dialogoTipoImportacion()
+		self.ui = Ui_dialogoTipoExportacion()
 		self.ui.setupUi(self)
 
-		self.ui.buttonImportar.clicked.connect(self.accept)
+		self.ui.buttonGuardar.clicked.connect(self.accept)
 		self.ui.buttonCancelar.clicked.connect(self.reject)
--- controladores/dialogos/dlgTipoImportacion.py
+++ controladores/dialogos/dlgTipoImportacion.py
@@ -1,16 +1,16 @@
 from PyQt6 import QtWidgets
 
-from ui.dlgTipoExportacion import Ui_dialogoTipoExportacion
+from ui.dlgTipoImportacion import Ui_dialogoTipoImportacion
 
 
 class DialogoTipoImportacion(QtWidgets.QDialog):
 	def __init__(self, clientes: bool, vehiculos: bool):
 		super(DialogoTipoImportacion, self).__init__()
-		self.ui = Ui_dialogoTipoExportacion()
+		self.ui = Ui_dialogoTipoImportacion()
 		self.ui.setupUi(self)
 
 		self.ui.checkboxClientes.setEnabled(clientes)
 		self.ui.checkboxCoches.setEnabled(vehiculos)
 
-		self.ui.buttonGuardar.clicked.connect(self.accept)
+		self.ui.buttonImportar.clicked.connect(self.accept)
 		self.ui.buttonCancelar.clicked.connect(self.reject)
