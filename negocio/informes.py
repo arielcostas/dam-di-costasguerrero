a34a4138bd5c870b17a3762ee2425b94c9adbbb9
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-01-10 14:30:12 +0100

    Empezar a hacer reportes

--- negocio/informes.py
+++ negocio/informes.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
+from reportlab.graphics.shapes import Line, Drawing
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import mm
-from reportlab.platypus import Table, SimpleDocTemplate, Paragraph, PageTemplate
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, SimpleDocTemplate, Paragraph
 
 from bbdd import Cliente
 
@@ -21,8 +25,21 @@ class Informes:
 			])
 
 		try:
-			doc = SimpleDocTemplate(ruta, pagesize=A4)
-			header = Paragraph("Listado de clientes", style="h1")
+			doc = canvas.Canvas(ruta)
+			doc.setPageSize(A4)
+
+			# Header
+			doc.line(30, 820, 570, 820)
+			doc.drawString(50, 805, "Talleres de Teis, S.L.")
+			doc.setFont("Helvetica", 7)
+			doc.drawString(50, 785, "CIF: 12345678A")
+			doc.drawString(50, 775, "Avenida de Galicia, 101")
+			doc.drawString(50, 765, "36208 Vigo - España")
+			doc.drawString(50, 755, "Teléfono: 986 986 986")
+			doc.drawString(50, 745, "a21arielcg@iesteis.es")
+			doc.line(30, 740, 570, 740)
+
+			doc.setFont("Helvetica", 12)
 
 			tabla = Table(data)
 			tabla.setStyle([
@@ -30,8 +47,8 @@ class Informes:
 				("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey)
 			])
 
-
-			doc.build([header, tabla])
+			doc.save()
+			#doc.build([header, tabla])
 		except Exception as e:
 			print(e)
 			return False
