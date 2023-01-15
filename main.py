8b581bac0ae497d5da850a98fcf518acdcd7dcb1
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-01-15 22:17:38 +0100

    añadir fuente manrope

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+from PyQt6.QtGui import QFontDatabase
 
 from controladores.ventmain import Main
 from PyQt6 import QtWidgets, QtGui
@@ -7,6 +10,13 @@ if __name__ == "__main__":
 	app = QtWidgets.QApplication([])
 	icon = QtWidgets.QFileIconProvider()
 	app.setWindowIcon(QtGui.QIcon("img/logo.png"))
+	app.setApplicationName("Talleres Teis")
+	app.setOrganizationName("Teis")
+	app.setApplicationDisplayName("Talleres Teis")
+
+	for font in os.listdir("fonts"):
+		QFontDatabase.addApplicationFont(f"fonts/{font}")
+	app.setFont(QtGui.QFont("Manrope", 9))
 	window = Main()
 	window.show()
 	sys.exit(app.exec())
